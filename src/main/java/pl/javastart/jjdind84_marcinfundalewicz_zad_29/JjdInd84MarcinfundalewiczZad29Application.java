070e31800450972eea73a8719c5d0420f1057583
Marcin Fundalewicz <marcin.fundalewicz@gmail.com>
2024-06-20 18:57:09 +0200

    Initial commit

---
+++ src/main/java/pl/javastart/jjdind84_marcinfundalewicz_zad_29/JjdInd84MarcinfundalewiczZad29Application.java
@@ -0,0 +1,13 @@
+package pl.javastart.jjdind84_marcinfundalewicz_zad_29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjdInd84MarcinfundalewiczZad29Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjdInd84MarcinfundalewiczZad29Application.class, args);
+    }
+
+}
